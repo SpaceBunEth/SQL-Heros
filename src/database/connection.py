fb9ae15cff83d11f11d1d1c25d8487e1cb5c7a6c
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-11-03 18:55:19 +0000

    created more sql statements, and python helper functions

--- src/database/connection.py
+++ src/database/connection.py
@@ -28,8 +28,12 @@ def execute_query(query, params=None):
         return cursor
     except OSError as e:
         print(f"The error '{e}' occurred or the hero name is already taken")
-
+#=================== DO NOT EDIT ABOVE =====================================
 #create_connection("postgres", "postgres", "postgres")
+
+
+#====================== PYTHON SQL =================================
+
 
 def select_all_heros():
     query = """
@@ -62,5 +66,76 @@ def select_heros_bio():
     list_of_bio = execute_query(query).fetchall()
     for record in list_of_bio:
         print(record[3])
-        
-select_all_heros_numbers()
+
+def select_ability_types():
+    query = """
+        SELECT * FROM ability_types
+    """
+    list_of_ability_types = execute_query(query).fetchall()
+    for record in list_of_ability_types:
+        print(record[1])
+
+#========= INSERT HERO ============
+def insert_new_hero():
+    input("Press Enter to Create a New Hero")
+    name = input('Enter Name: ')
+    about = input('Write About Me for Hero: ')
+    bio = input('Write a bio for your Hero: ')
+
+    query = """
+        INSERT INTO heroes 
+            (name,about_me,biography)
+        VALUES (%s,%s,%s);
+    """    
+    execute_query(query,(name,about,bio)) 
+    #query,(name,) 
+
+#========== REMOVE HERO ===============
+#DELETE FROM table_name WHERE condition;
+
+def delete_hero():
+    input('Press Enter to see Hero Numbers')
+    command_dict['All_Numbers']()
+    hero_id = input('Delete Hero by Number: ')
+    query = """
+        DELETE FROM heroes WHERE id = %s
+    """
+    execute_query(query,(hero_id,))
+
+#========= Heros & Ability =======
+def select_hero_ability_type():
+    query = """
+        SELECT DISTINCT heroes.id,heroes.name, string_agg(ability_types.name,' ')
+        FROM heroes
+        JOIN abilities ON heroes.id = abilities.hero_id
+        JOIN ability_types ON ability_types.id = abilities.ability_type_id
+        GROUP BY heroes.id   
+    """
+    list_of_heros_ability = execute_query(query).fetchall()
+    for record in list_of_heros_ability:
+        print('\n',record[0],'  ',record[1],'  ',record[2])
+    print('\n')
+#======== Command list ============         
+
+command_dict = {
+    'All': select_all_heros,
+    'All_Numbers': select_all_heros_numbers,
+    'All_About': select_heros_about,
+    'All_Bio': select_heros_bio,
+    'Ability_Types': select_ability_types,
+    'New_Hero': insert_new_hero,
+    'Delete_Hero': delete_hero,
+    'Hero_Ability': select_hero_ability_type
+}
+# command_dict[command]()
+def input_func():
+    command = input("ENTER COMMAND: ")
+    for key in command_dict:
+        if command in command_dict:
+            command_dict[command]()
+            input_func()
+        else:
+            print('---- Not A Valid Command -----')
+            input_func()
+
+input_func()
